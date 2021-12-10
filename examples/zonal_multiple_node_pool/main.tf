736290d2d984d822676719d561d5b534c6917a42
Pierre Averous <piaverous@gmail.com>
2021-12-10 16:49:04 +0100

    feat: add example for zonal private cluster

--- examples/zonal_multiple_node_pool/main.tf
+++ examples/zonal_multiple_node_pool/main.tf
@@ -9,8 +9,8 @@ module "custom_service_account" {
   source = "git@github.com:padok-team/terraform-google-serviceaccount.git"
 
   list_serviceaccount = {
-    gke-sa = {
-      predifined_roles = []
+    gke-zonal-test-sa = {
+      predifined_roles = ["roles/compute.admin", "roles/container.admin"]
       permissions = [
         "container.clusters.create",
         "container.clusters.delete",
@@ -27,41 +27,56 @@ module "custom_service_account" {
 module "custom_network" {
   source = "git@github.com:padok-team/terraform-google-network.git"
 
-  name = "my-super-duper-cluster-network"
+  name = "my-zonal-super-duper-cluster-network"
   subnets = {
-    "kubernetes-nodes" = {
+    "zonal-kubernetes-nodes" = {
       cidr   = "10.21.0.0/16"
       region = "europe-west1"
     }
   }
 }
 
-module "regional_cluster_use_case" {
+module "zonal_cluster_use_case" {
   source = "../.."
 
-  name     = "my-regional-cluster"
-  location = "europe-west1"
+  name     = "my-zonal-cluster"
+  location = "europe-west1-b"
 
   node_service_account = {
-    email = module.custom_service_account.service_account_email["gke-sa"]
+    email = module.custom_service_account.service_account_email["gke-zonal-test-sa"]
   }
+  ip_whitelist_master_network = [
+    { "name" = "pierrea", "cidr" = "31.32.227.74/32" },
+    { "name" = "kims", "cidr" = "82.216.204.48/32" }
+  ]
 
-  cidr_master = "10.168.0.0/28"
+  cidr_master = "10.168.1.0/28"
   network = {
     id = "projects/padok-playground/global/networks/${module.custom_network.network.name}"
   }
-  subnetwork = module.custom_network.network.subnets["kubernetes-nodes"] # Reference the subnet created above
+  subnetwork = module.custom_network.network.subnets["zonal-kubernetes-nodes"] # Reference the subnet created above
 
   node_pools = {
-    "classy-node-pool" = {
+    "first-zonal-node-pool" = {
       min_size     = 1
       max_size     = 2
       machine_type = "e2-micro"
       preemptible  = true
+    },
+    "second-zonal-node-pool" = {
+      min_size     = 1
+      max_size     = 3
+      machine_type = "e2-micro"
+      preemptible  = false
     }
   }
 }
 
 output "service_account_output" {
-  value = module.custom_service_account.service_account_email["gke-sa"]
+  value = module.custom_service_account.service_account_email["gke-zonal-test-sa"]
+}
+
+output "kubernetes_cluster" {
+  value     = module.zonal_cluster_use_case.kubernetes_cluster
+  sensitive = true
 }
