c8983a6923387082d0cc91307c403a2dde01ee10
Alan <alanl@padok.fr>
2022-05-27 15:59:02 +0200

    feat(versions): setup versionning as edicted by padok guidelines

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = "~> 1.0"
 
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 3.90, < 5.0.0"
+      version = "~> 4.21"
     }
   }
 }
